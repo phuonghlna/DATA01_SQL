aa16f4703811f633d979dba3f043c2569093ca5c
Michael Phuong <88137436+phuonghlna@users.noreply.github.com>
2023-10-19 06:16:08 +0700

    Update Day07.sql

--- Day07.sql
+++ Day07.sql
@@ -6,3 +6,27 @@ WHERE Marks > 75
 ORDER BY RIGHT(Name,3), ID
 
 -- Ex02
+SELECT
+user_id,
+CONCAT(UPPER(LEFT(name,1)), LOWER(SUBSTRING(name,2))) AS name
+FROM Users
+ORDER BY user_id
+
+-- Ex03
+SELECT
+manufacturer,
+CONCAT('$', ROUND(SUM(total_sales)/1000000), ' million') AS sale
+FROM pharmacy_sales
+GROUP BY manufacturer
+ORDER BY  SUM(total_sales) DESC, manufacturer
+
+-- Ex04
+SELECT
+EXTRACT(MONTH FROM submit_date) as mth,
+product_id,
+ROUND(AVG(stars),2) AS avg_stars
+FROM reviews
+GROUP BY EXTRACT(MONTH FROM submit_date), product_id
+ORDER BY mth, product_id
+
+-- Ex05
