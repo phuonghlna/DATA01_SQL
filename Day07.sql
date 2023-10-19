49b9f2994d3389cb2e59257947650511ee0133e2
Michael Phuong <88137436+phuonghlna@users.noreply.github.com>
2023-10-19 21:59:16 +0700

    Update Day07.sql

--- Day07.sql
+++ Day07.sql
@@ -30,3 +30,35 @@ GROUP BY EXTRACT(MONTH FROM submit_date), product_id
 ORDER BY mth, product_id
 
 -- Ex05
+SELECT
+sender_id, COUNT(message_id) AS count_mesages
+FROM messages
+WHERE EXTRACT(MONTH FROM sent_date) ='8'
+AND EXTRACT(YEAR FROM sent_date) = '2022' 
+GROUP BY sender_id
+ORDER BY COUNT(message_id) DESC
+LIMIT 2
+
+-- Ex06
+SELECT tweet_id
+FROM Tweets
+WHERE LENGTH(content) > 15
+
+-- Ex07
+ECT activity_date AS day,
+COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-27' -30 AND '2019-07-27'
+GROUP BY activity_date
+
+-- Ex08
+
+-- Ex09
+select position('a' IN first_name)
+from worker
+WHERE first_name ='Amitah'
+
+-- Ex10
+select title,
+substring(title,length(winery) +2, 4) as vintage_year
+from winemag_p2;
