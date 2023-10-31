994ebe77670729b05b542abcfff72cfabbb75c1f
Michael Phuong <88137436+phuonghlna@users.noreply.github.com>
2023-10-31 07:04:59 +0700

    Update Day13.sql

--- Day13.sql
+++ Day13.sql
@@ -13,3 +13,27 @@ FROM job_count_cte
 WHERE job_count > 1
 
 -- Ex02
+WITH ranked_spending_cte AS (
+  SELECT 
+    category, 
+    product, 
+    SUM(spend) AS total_spend,
+    RANK() OVER (
+      PARTITION BY category 
+      ORDER BY SUM(spend) DESC) AS ranking 
+  FROM product_spend
+  WHERE EXTRACT(YEAR FROM transaction_date) = 2022
+  GROUP BY category, product
+)
+
+SELECT 
+  category, 
+  product, 
+  total_spend 
+FROM ranked_spending_cte 
+WHERE ranking <= 2 
+ORDER BY category, ranking;
+
+-- Ex03
+
+
