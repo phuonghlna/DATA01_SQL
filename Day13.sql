f956228a4977c96fc67cb2c249eed622f25de7f1
Michael Phuong <88137436+phuonghlna@users.noreply.github.com>
2023-11-02 07:19:37 +0700

    Update Day13.sql

--- Day13.sql
+++ Day13.sql
@@ -53,4 +53,33 @@ SELECT p.page_id
 FROM pages p JOIN page_like_id pl
 ON p.page_id = pl.page_id
 
+-- Ex05
+SELECT
+EXTRACT(MONTH FROM curr_month.event_date) AS mth,
+COUNT(DISTINCT curr.month_user_id) AS monthly_active_users
+FROM user_actions AS curr_month
+WHERE EXISTS(
+SELECT last_month.user_id
+FROM user_actions AS last_month
+WHERE last_month.user_id = curr_month.user_id
+AND EXTRACT(MONTH FROM last_month.event_date) =
+EXTRACT(MONTH FROM curr_month.even_date - interval '1 month')
+)
+AND EXTRACT(MONTH FROM curr_month,event_date) = 7
+AND EXTRACT(YEAR FROM curr_month.event_date) = 2022
+GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
+
+-- Ex06
+SELECT DATE_FORMAT(trans_date, '%Y-%m') 
+AS month,
+       country,
+       COUNT(id) AS trans_count,
+       SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+       SUM(amount) AS trans_total_amount,
+       SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country;
+
+-- Ex07
+
 
