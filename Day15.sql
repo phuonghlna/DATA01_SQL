7a52fca103adaaa9f6c1529d9e2342a2ecb8017f
Michael Phuong <88137436+phuonghlna@users.noreply.github.com>
2023-11-07 06:56:39 +0700

    Create Day15.sql

---
+++ Day15.sql
@@ -0,0 +1,21 @@
+-- Ex01
+WITH twt_yearly_spend AS(
+SELECT
+EXTRACT(YEAR FROM transaction_date) AS yr,
+product_id,
+spend AS curr_year_spend,
+LAG(spend) OVER (PARTITION BY product_id ORDER BY
+product_id, EXTRACT(YEAR FROM transaction_date)) AS prev_year_spend 
+FROM user_transactions
+)
+
+SELECT
+yr,
+product_id,
+curr_year_spend, 
+prev_year_spend,
+ROUND(100 * (curr_year_spend - prev_year_spend)
+    / prev_year_spend, 2) AS yoy_rate
+FROM twt_yearly_spend
+
+-- Ex02
